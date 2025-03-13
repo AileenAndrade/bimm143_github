1ebed837d0e2b7b170d5916f920706fed74a88a5
Aileen Andrade <aiandrade@ucsd.edu>
2025-03-13 12:01:45 -0700

    Added the original class folders (Class0#)

---
+++ Class04/BIMM 143 Class 4.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: Aileen Andrade (PID A17033749)
+#' ---
+
+
+x <- 1:50
+plot(x,sin(x))
+
+# Make this nicer
+plot(x, sin(x), col="blue", type="l", lwd=3)
